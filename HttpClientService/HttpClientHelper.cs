3898527453108e0530dd06167a52c5e0bcc89c51
Mpucuko Ndlazi <MpucukoNdlazi@gmail.com>
2024-04-17 17:27:50 +0200

    HttpClient helper clean up

--- HttpClientService/HttpClientHelper.cs
+++ HttpClientService/HttpClientHelper.cs
@@ -1,123 +1,72 @@
-﻿using HttpClientLibrary.Helpers;
-using HttpClientLibrary.Interface;
-using HttpClientLibrary.Model;
+﻿using HttpClientLibrary.Model;
 using System.Net.Http.Json;
 
 namespace HttpClientLibrary.HttpClientService
 {
-	public class HttpClientHelper<T>(IHttpClientFactory httpClientFactory) : IHttpClientHelper<T>
-	{
-		public async Task<List<T>> HttpGetAllAsync(string RequestUri)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
+    public class HttpClientHelper(HttpClient _httpClient) : IHttpClientHelper
+    {
+        public async Task<List<T>> HttpRetrieveAllAsync<T>(string RequestUri)
+        {
+            var httpResponse = await _httpClient.GetAsync(RequestUri);
+            httpResponse.EnsureSuccessStatusCode();
 
-			var response = await _httpClient.GetAsync(RequestUri);
+            var result = await httpResponse.Content.ReadFromJsonAsync<List<T>>();
 
-			var validatedResponse = response.ValidateResponse(RequestUri, Helper.GET);
+            return result!;
+        }
 
-			var result = await validatedResponse.Content.ReadFromJsonAsync<List<T>>();
+        public async Task<T> HttpRetrieveByIdAsync<T>(string RequestUri)
+        {
+            var httpResponse = await _httpClient.GetAsync(RequestUri);
+            httpResponse.EnsureSuccessStatusCode();
 
-			return result!;
-		}
+            var result = await httpResponse.Content.ReadFromJsonAsync<T>();
 
-		public async Task<List<U>> HttpGetAllAsync<U>(string RequestUri)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
+            return result!;
+        }
 
-			var response = await _httpClient.GetAsync(RequestUri);
-			var validatedResponse = response.ValidateResponse(RequestUri, Helper.GET);
+        public async Task<HttpResponseMessage> HttpPostAsync<T>(string RequestUri, T model)
+        {
+            var httpResponse = await _httpClient.PostAsJsonAsync(RequestUri, model);
+            httpResponse.EnsureSuccessStatusCode();
 
-			var result = await validatedResponse.Content.ReadFromJsonAsync<List<U>>();
+            return httpResponse;
+        }
 
-			return result!;
-		}
+        public async Task<HttpResponseMessage> HttpPutAsync<T>(string RequestUri, T model)
+        {
+            var httpResponse = await _httpClient.PutAsJsonAsync(RequestUri, model);
+            httpResponse.EnsureSuccessStatusCode();
 
-		public async Task<T> HttpGetByIdAsync(string RequestUri)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
+            return httpResponse;
+        }
 
-			var response = await _httpClient.GetAsync(RequestUri);
+        public async Task<HttpResponseMessage> HttpDeleteAsync(string RequestUri)
+        {
+            var httpResponse = await _httpClient.DeleteAsync(RequestUri);
+            httpResponse.EnsureSuccessStatusCode();
 
-			var validatedResponse = response.ValidateResponse(RequestUri, Helper.GET);
+            return httpResponse;
+        }
 
-			var result = await validatedResponse.Content.ReadFromJsonAsync<T>();
+        public async Task<HttpResponseMessage> HttpRetrieveAccessTokenAsync(AccessTokenModel model)
+        {
+            List<KeyValuePair<string, string>> keyValues =
+                 [
+                     new KeyValuePair<string, string>("username",model.Username),
+                    new KeyValuePair<string, string>("password",model.Password),
+                    new KeyValuePair<string, string>("grant_type",model.GrantType)
+                 ];
 
-			return result!;
-		}
+            HttpRequestMessage request = new(HttpMethod.Post, model.RequestUrl)
+            {
+                Content = new FormUrlEncodedContent(keyValues)
+            };
 
-		public async Task<U> HttpGetByIdAsync<U>(string RequestUri)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
 
-			var response = await _httpClient.GetAsync(RequestUri);
-
-			var validatedResponse = response.ValidateResponse(RequestUri, Helper.GET);
-
-			var result = await validatedResponse.Content.ReadFromJsonAsync<U>();
-
-			return result!;
-		}
-
-		public async Task<HttpResponseMessage> HttpPostAsync(string RequestUri, T model)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
-
-			HttpResponseMessage response = await _httpClient.PostAsJsonAsync(RequestUri, model);
-			return response.ValidateResponse(RequestUri, Helper.POST);
-		}
-
-		public async Task<HttpResponseMessage> HttpPostAsync<U>(string RequestUri, U model)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
-
-			HttpResponseMessage response = await _httpClient.PostAsJsonAsync(RequestUri, model);
-			return response.ValidateResponse(RequestUri, Helper.POST);
-		}
-
-		public async Task<HttpResponseMessage> HttpPutAsync(string RequestUri, T model)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
-
-			HttpResponseMessage response = await _httpClient.PutAsJsonAsync(RequestUri, model);
-			return response.ValidateResponse(RequestUri, Helper.PUT);
-		}
-
-		public async Task<HttpResponseMessage> HttpPutAsync<U>(string RequestUri, U model)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
-
-			HttpResponseMessage response = await _httpClient.PutAsJsonAsync(RequestUri, model);
-			return response.ValidateResponse(RequestUri, Helper.PUT);
-		}
-
-		public async Task<HttpResponseMessage> HttpDeleteAsync(string RequestUri)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
-
-			HttpResponseMessage response = await _httpClient.DeleteAsync(RequestUri);
-			return response.ValidateResponse(RequestUri, Helper.DELETE);
-		}
-
-		public async Task<HttpResponseMessage> HttpGetAccessTokenAsync(AccessTokenModel model)
-		{
-			var _httpClient = httpClientFactory.CreateClient(Helper.HttpClientName);
-
-			List<KeyValuePair<string, string>> keyValues =
-				[
-					new KeyValuePair<string, string>("username",model.Username),
-					new KeyValuePair<string, string>("password",model.Password),
-					new KeyValuePair<string, string>("grant_type",model.GrantType)
-				];
-
-			HttpRequestMessage request = new(HttpMethod.Post, model.RequestUrl)
-			{
-				Content = new FormUrlEncodedContent(keyValues)
-			};
-
-			HttpResponseMessage response = await _httpClient.SendAsync(request);
-			response.EnsureSuccessStatusCode();
-
-			return response;
-		}
-	}
+            return response;
+        }
+    }
 }
---
+++ HttpClientService/IHttpClientHelper.cs
@@ -0,0 +1,15 @@
+﻿using HttpClientLibrary.Model;
+
+namespace HttpClientLibrary.HttpClientService
+{
+    public interface IHttpClientHelper
+    {
+        Task<HttpResponseMessage> HttpDeleteAsync(string RequestUri);
+        Task<HttpResponseMessage> HttpRetrieveAccessTokenAsync(AccessTokenModel model);
+        Task<List<T>> HttpRetrieveAllAsync<T>(string RequestUri);
+        Task<T> HttpRetrieveByIdAsync<T>(string RequestUri);
+        Task<HttpResponseMessage> HttpPostAsync<T>(string RequestUri, T model);
+        Task<HttpResponseMessage> HttpPutAsync<T>(string RequestUri, T model);
+
+    }
+}
