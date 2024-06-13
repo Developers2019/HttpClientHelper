4b448ef9bb25be168c278239471e08794036327b
Mpucuko Ndlazi <mpucukon@hollywoodbet.net>
2024-06-13 13:37:56 +0200

    Included xml documentation on all the methods.

--- HttpClientService/HttpClientHelper.cs
+++ HttpClientService/HttpClientHelper.cs
@@ -5,6 +5,12 @@ namespace HttpClientLibrary.HttpClientService
 {
     public class HttpClientHelper(HttpClient _httpClient) : IHttpClientHelper
     {
+        /// <summary>
+        /// Retrieves a list of items from the specified URI asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of items to retrieve.</typeparam>
+        /// <param name="RequestUri">The URI of the resource to retrieve.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of items of type <typeparamref name="T"/>.</returns>
         public async Task<List<T>> HttpRetrieveAllAsync<T>(string RequestUri)
         {
             var httpResponse = await _httpClient.GetAsync(RequestUri);
@@ -15,6 +21,12 @@ namespace HttpClientLibrary.HttpClientService
             return result!;
         }
 
+        /// <summary>
+        /// Retrieves an item by ID from the specified URI asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of item to retrieve.</typeparam>
+        /// <param name="RequestUri">The URI of the resource to retrieve.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains an item of type <typeparamref name="T"/>.</returns>
         public async Task<T> HttpRetrieveByIdAsync<T>(string RequestUri)
         {
             var httpResponse = await _httpClient.GetAsync(RequestUri);
@@ -25,6 +37,13 @@ namespace HttpClientLibrary.HttpClientService
             return result!;
         }
 
+        /// <summary>
+        /// Sends a POST request with a model to the specified URI asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of the model to send.</typeparam>
+        /// <param name="RequestUri">The URI of the resource to send the request to.</param>
+        /// <param name="model">The model to send in the request body.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         public async Task<HttpResponseMessage> HttpPostAsync<T>(string RequestUri, T model)
         {
             var httpResponse = await _httpClient.PostAsJsonAsync(RequestUri, model);
@@ -33,6 +52,13 @@ namespace HttpClientLibrary.HttpClientService
             return httpResponse;
         }
 
+        /// <summary>
+        /// Sends a PUT request with a model to the specified URI asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of the model to send.</typeparam>
+        /// <param name="RequestUri">The URI of the resource to send the request to.</param>
+        /// <param name="model">The model to send in the request body.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         public async Task<HttpResponseMessage> HttpPutAsync<T>(string RequestUri, T model)
         {
             var httpResponse = await _httpClient.PutAsJsonAsync(RequestUri, model);
@@ -41,6 +67,11 @@ namespace HttpClientLibrary.HttpClientService
             return httpResponse;
         }
 
+        /// <summary>
+        /// Sends a DELETE request to the specified URI asynchronously.
+        /// </summary>
+        /// <param name="RequestUri">The URI of the resource to delete.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         public async Task<HttpResponseMessage> HttpDeleteAsync(string RequestUri)
         {
             var httpResponse = await _httpClient.DeleteAsync(RequestUri);
@@ -49,14 +80,19 @@ namespace HttpClientLibrary.HttpClientService
             return httpResponse;
         }
 
+        /// <summary>
+        /// Retrieves an access token using the specified access token model asynchronously.
+        /// </summary>
+        /// <param name="model">The access token model containing the necessary parameters.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         public async Task<HttpResponseMessage> HttpRetrieveAccessTokenAsync(AccessTokenModel model)
         {
-            List<KeyValuePair<string, string>> keyValues =
-                 [
-                     new KeyValuePair<string, string>("username",model.Username),
-                    new KeyValuePair<string, string>("password",model.Password),
-                    new KeyValuePair<string, string>("grant_type",model.GrantType)
-                 ];
+            List<KeyValuePair<string, string>> keyValues = 
+            [
+                new KeyValuePair<string, string>("username", model.Username),
+                new KeyValuePair<string, string>("password", model.Password),
+                new KeyValuePair<string, string>("grant_type", model.GrantType)
+            ];
 
             HttpRequestMessage request = new(HttpMethod.Post, model.RequestUrl)
             {
@@ -68,5 +104,6 @@ namespace HttpClientLibrary.HttpClientService
 
             return response;
         }
+
     }
 }
--- HttpClientService/IHttpClientHelper.cs
+++ HttpClientService/IHttpClientHelper.cs
@@ -4,11 +4,52 @@ namespace HttpClientLibrary.HttpClientService
 {
     public interface IHttpClientHelper
     {
+        /// <summary>
+        /// Sends a DELETE request to the specified URI asynchronously.
+        /// </summary>
+        /// <param name="RequestUri">The URI of the resource to delete.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         Task<HttpResponseMessage> HttpDeleteAsync(string RequestUri);
+
+        /// <summary>
+        /// Retrieves an access token using the specified access token model asynchronously.
+        /// </summary>
+        /// <param name="model">The access token model containing the necessary parameters.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         Task<HttpResponseMessage> HttpRetrieveAccessTokenAsync(AccessTokenModel model);
+
+        /// <summary>
+        /// Retrieves a list of items from the specified URI asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of items to retrieve.</typeparam>
+        /// <param name="RequestUri">The URI of the resource to retrieve.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of items of type <typeparamref name="T"/>.</returns>
         Task<List<T>> HttpRetrieveAllAsync<T>(string RequestUri);
+
+        /// <summary>
+        /// Retrieves an item by ID from the specified URI asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of item to retrieve.</typeparam>
+        /// <param name="RequestUri">The URI of the resource to retrieve.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains an item of type <typeparamref name="T"/>.</returns>
         Task<T> HttpRetrieveByIdAsync<T>(string RequestUri);
+
+        /// <summary>
+        /// Sends a POST request with a model to the specified URI asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of the model to send.</typeparam>
+        /// <param name="RequestUri">The URI of the resource to send the request to.</param>
+        /// <param name="model">The model to send in the request body.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         Task<HttpResponseMessage> HttpPostAsync<T>(string RequestUri, T model);
+
+        /// <summary>
+        /// Sends a PUT request with a model to the specified URI asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of the model to send.</typeparam>
+        /// <param name="RequestUri">The URI of the resource to send the request to.</param>
+        /// <param name="model">The model to send in the request body.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         Task<HttpResponseMessage> HttpPutAsync<T>(string RequestUri, T model);
 
     }
